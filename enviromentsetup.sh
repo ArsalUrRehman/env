74fbf7fb1236575580ec172e18fc62deb3d18639
Arsal Ur Rehman <sarah.smith@email.com>
2022-09-29 12:01:45 +0500

    fix

--- enviromentsetup.sh
+++ enviromentsetup.sh
@@ -3,10 +3,27 @@
 echo "################################################################################"
 echo "Installing Virtual Box"
 echo "################################################################################"
-sudo apt-get update
-sudo apt-get install software–properties–common
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
-sudo apt-get update
-sudo apt-get install virtualbox -y
+#sudo apt-get update
+#sudo apt-get install software–properties–common
+#sudo apt-get update
+#sudo apt install virtualbox-6.1 -y
+#sudo apt install virtualbox-ext-pack -y
 echo "#################################################################################"
+
+echo "##################################################################################"
+echo "Installing Postman"
+echo "##################################################################################"
+sudo apt-get update
+sudo snap install postman
+echo "##################################################################################"
+
+echo "##################################################################################"
+echo"Installing Anydesk"
+echo "##################################################################################"
+sudo apt update
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt update
+sudo apt install anydesk
+echo "##################################################################################"
+
